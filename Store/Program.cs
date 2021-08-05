0f6fd7227a41479a67e3a1e4d1846e6eb78f3231
Kirill <nikodakirill386@gmail.com>
2021-08-05 23:53:16 +0300

    added products controller & product entity

--- Store/Program.cs
+++ Store/Program.cs
@@ -9,14 +9,15 @@ using Microsoft.Extensions.Logging;
 
 namespace Store
 {
-    public class Program
+    
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
@@ -24,4 +25,3 @@ namespace Store
                 });
     }
 }
-
